d623d478160167aac213e5ff9b2d9c4ca56072d2
Ashutosh-900 <ashutoshkumar2364@gmail.com>
2026-08-09 21:59:27 +0530

    Adding program of rotate array in c++.

---
+++ 01_BASICS_CPP/PROGRAMS-USING-VECTOR/rotateArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void rotate(vector<int> &nums , int k){
+    vector<int> temp (nums.size());
+    for(int i =0 ; i<nums.size() ; i++){
+        temp[(i+k)%nums.size()] = nums[i];
+
+    }
+    nums = temp;
+}
+void printarray(vector<int>nums ){
+    for (int i = 0 ; i<nums.size() ; i++ ){
+        cout << nums[i]<< " ";
+    }
+    cout<<endl;
+
+}
+int main () {
+    vector <int> nums;
+    nums.push_back(1);
+    nums.push_back(2);
+    nums.push_back(3);
+    nums.push_back(4);
+    rotate(nums,2);
+    printarray(nums);
+}
