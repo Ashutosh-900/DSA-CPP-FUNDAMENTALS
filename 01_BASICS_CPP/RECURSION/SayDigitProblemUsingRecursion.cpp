50c218a6a5c6f075124eae7410075e365b81cdf2
Ashutosh-900 <ashutoshkumar2364@gmail.com>
2026-08-25 22:07:05 +0530

    Adding code for saydigit problem using recursion in c++.

---
+++ 01_BASICS_CPP/RECURSION/SayDigitProblemUsingRecursion.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+void saydigit(int n, string arr[]){
+    //base case 
+    if(n == 0)
+    return;
+    //processing
+    int digit = n%10;
+    n = n/10;
+    // recursive call
+    saydigit(n,arr);
+    cout<< arr[digit]<<" ";
+
+}
+int main () {
+    string arr[10] = {"zero", "one" ,"two" ,"three", "four" ,"five" ,"six","seven","eight","nine"};
+    int n;
+    cout<< "enter the number you want :"<<endl;
+    cin>>n;
+    cout<<endl;
+    saydigit(n,arr);
+    cout<<endl<<endl;
+    return 0;
+
+
+
+
+}
