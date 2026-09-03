20919567c408a4899daf11778bb519a855fb4af4
Ashutosh-900 <ashutoshkumar2364@gmail.com>
2026-09-03 23:43:56 +0530

    Adding quicksort algorithm using recursion in my repository. This implementation of quicksort uses the divide-and-conquer approach to sort an array of integers. The algorithm selects a 'pivot' element from the array and partitions the other elements into two sub-arrays according to whether they are less than or greater than the pivot. The sub-arrays are then sorted recursively.

---
+++ 01_BASICS_CPP/RECURSION/QuickSort_UsingRecursion.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+int partition(int arr[] , int s ,int e){
+    int pivot=arr[s];
+    int cnt = 0;
+    for(int i = s+1 ; i<=e ; i++){
+        if(arr[i]<=pivot){
+        cnt++;
+        }
+    }
+    int pivotindex = s + cnt;
+    swap(arr[pivotindex],arr[s]);
+    //left and right part dekh kai
+    int i=s,j=e;
+    while(i<pivotindex && j>pivotindex){
+        while(arr[i]<pivot){
+            i++;
+        }
+        while(arr[j]>pivot){
+            j--;
+        }
+        if(i<pivotindex && j>pivotindex){
+            swap(arr[i++],arr[j--]);
+        }
+    }
+    return pivotindex;
+}
+void quicksort(int arr[] , int s , int e){
+    //base case
+    if(s>=e)
+    return;
+    //partition
+    int p  = partition(arr,s,e);
+    //recursive call
+    //left part sort
+    quicksort(arr,s,p-1);
+    //right part sort
+    quicksort(arr,p+1,e);
+}
+int main () {
+    int arr[7] ={10,80,30,90,40,50,70};
+    int n=7;
+    quicksort(arr,0,n-1);
+    //printing the sorted array
+    for(int i = 0 ; i<n ; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0 ;
+}
