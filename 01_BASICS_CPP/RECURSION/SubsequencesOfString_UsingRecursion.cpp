70e58647d0e5902e85c271e175acb385fb84773d
Ashutosh-900 <ashutoshkumar2364@gmail.com>
2026-09-05 22:38:08 +0530

    Adding a new C++ file that implements a function to generate all subsequences of a given string using recursion. This implementation will help in understanding the concept of recursion and how it can be applied to solve problems related to strings.

---
+++ 01_BASICS_CPP/RECURSION/SubsequencesOfString_UsingRecursion.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void solve(string str , string output , int index , vector<string> &ans){
+    //base case
+    if(index>= str.length()){
+        if(output.length() > 0){
+            ans.push_back(output);
+
+        }
+        return;
+    }
+    //exclude
+    solve(str, output, index+1 ,ans);
+    //include 
+    char element  = str[index];
+    output.push_back(element);
+    solve(str,output,index+1 ,ans);
+}
+vector <string> subsequences(string str){
+    vector <string> ans; 
+    string output = "";
+    int index= 0 ;
+    solve(str, output ,index , ans);
+    return ans;
+}
+int main () {
+    string str = "abc";
+    vector <string> finalans=subsequences(str);
+    for(string i : finalans){
+        cout<< i << " ";
+    }
+    cout<<endl;
+    return 0;
+}
